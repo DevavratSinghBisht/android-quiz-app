47a0981eabb3321e359d9a95e837b56f88ebfb51
devavratsinghbisht <devavratsinghbisht@gmail.com>
2021-03-29 21:21:09 +0530

    Dev and Pratik work done!
    1. Animation completed
    2. Successfully connected to Firebase

---
+++ app/src/main/java/com/example/quizzerrr/CategoryModel.java
@@ -0,0 +1,41 @@
+package com.example.quizzerrr;
+
+public class CategoryModel {
+    private String name;
+    private int sets;
+    private String url;
+
+    public CategoryModel(){
+        //for firebase
+    }
+
+    public CategoryModel(String name, String url, int sets) {
+        this.name = name;
+        this.url = url;
+        this.sets = sets;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public int getSets() {
+        return sets;
+    }
+
+    public void setSets(int sets) {
+        this.sets = sets;
+    }
+}
